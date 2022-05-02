40723a46816213f8a408d3c5122e7da57cebac33
Atanseiye <92696803+Atanseiye@users.noreply.github.com>
2022-05-02 11:34:27 +0100

    Add files via upload

---
+++ polargrid.js
@@ -0,0 +1,13 @@
+gsap.fromTo('.text', {y: 100}, {y: 0, duration: 2})
+
+gsap.fromTo('.battery', {x: 500, opacity: .5}, {x: 0, opacity: 1, duration: 2})
+
+// Social Icons
+gsap.fromTo('.contactpan', {y: 100, opacity: .5}, {y: 0, opacity: 1, duration: 2})
+gsap.fromTo('.socialIcon', {y: 100, opacity: .5}, {y: 0, opacity: 1, duration: 2})
+
+
+// third
+gsap.fromTo('.battery3', {x: -500, opacity: .5}, {x: 0, opacity: 1, duration: 2})
+
+gsap.fromTo('.text2', {y: 100}, {y: 0, duration: 2})
